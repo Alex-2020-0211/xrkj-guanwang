1782a046afcbca8c949e5d9425234a53f0febbe2
木易 <mishi_address@sina.com>
2021-01-09 15:39:00 +0800

    xrkj-third

---
+++ .history/src/views/CustomerCase_20210108143110.vue
@@ -0,0 +1,485 @@
+<template>
+  <div class="customercase">
+    <div class="border">
+      <!-- 头部 -->
+      <div class="header">
+        <div class="headerTop">
+          <p class="bar">
+            <img class="logo" src="../assets/img/logo.png" alt />
+            <a href="http://localhost:8080/#/">首页</a>
+            <a href="http://localhost:8080/#/productintroduction">产品介绍</a>
+            <a href="http://localhost:8080/#/solutions">解决方案</a>
+            <a href="http://localhost:8080/#/customercase">客户案例</a>
+            <a href="http://localhost:8080/#/aboutus">关于</a>
+          </p>
+          <button class="mianfei">免费试用</button>
+        </div>
+        <div class="headerMain">
+          <div class="theme">
+            <dl>
+              <dt>“ 对工作上瘾? ”</dt>
+              <dd>让销售上瘾，让团队带劲</dd>
+            </dl>
+            <button class="knowMore" @click="goAbout()">了解一下</button>
+          </div>
+        </div>
+      </div>
+      <!-- 选择NICETEAM的理由 -->
+      <div class="reason">
+        <!-- top -->
+        <div class="reasonT">
+          <h5>他们选择NICETEAM的理由</h5>
+          <p class="question">
+            随着90后、00后进入职场，中小企业在员工管理方面遇到了新的难题和痛点，并阻碍了企业的发展。
+          </p>
+          <div class="case">
+            <img src="../assets/img/case-1.png" />
+            <div class="told">
+              <p class="fs50 left">“</p>
+              <p class="left">我从来没想过，管理会这么有趣且高效!</p>
+              <p class="fs50 right">”</p>
+              <p class="person" style="margin-top: 0.4rem;">
+                <strong>李天昊</strong><span>盛利金融</span>
+              </p>
+            </div>
+          </div>
+          <div class="case">
+            <div class="told">
+              <p class="fs50 left">“</p>
+              <p class="left">
+                从0到1，NICETEAM给予了我太大的帮助，游戏化管理的模式让我从怀疑到信赖到今天的喜欢
+              </p>
+              <p class="fs50 right">”</p>
+              <p class="person">
+                <strong>陈慧慧</strong><span>喜盈门科技</span>
+              </p>
+            </div>
+            <img src="../assets/img/case-2.png" />
+          </div>
+        </div>
+        <!-- middle -->
+        <div class="reasonM">
+          <div class="content">
+            <div class="content-item" style="margin-left: 0.5rem;">
+              <p class="circle-item"></p>
+              <dl>
+                <dt>专业的团队</dt>
+                <dd>提供专业的技术团队和设计团队为用户提供周全的保障</dd>
+              </dl>
+            </div>
+            <div class="content-item">
+              <p class="circle-item"></p>
+              <dl>
+                <dt>优质的商家</dt>
+                <dd>严选品牌供应商，放大福利价值，提升员工对福主观能动性。</dd>
+              </dl>
+            </div>
+            <div class="content-item" style="margin-right: 0.55rem;">
+              <p class="circle-item"></p>
+              <dl>
+                <dt>销售行为实时采集</dt>
+                <dd>
+                  SCRM销售场景的数据采集为你研判员工和客户提供精准数据支撑，有效提高业绩
+                </dd>
+              </dl>
+            </div>
+          </div>
+          <div class="reason-box">
+            <div class="reason-box-item item1"></div>
+            <div class="reason-box-item"></div>
+            <div class="reason-box-item item3"></div>
+            <div class="reason-box-item item4"></div>
+          </div>
+        </div>
+        <!-- bottom -->
+        <div class="reasonB">
+          <h5>源于客户信赖</h5>
+          <p class="zhuli">源于用户信赖，助力企业高效管理</p>
+          <div class="accordion">
+            <!-- <div class="accordion-item">
+              <p>
+                从0到1，NICETEAM给予了我太大的帮助，游戏化管理的模式让我从怀疑到信赖到今天的喜欢，团队业绩越来越好，员工也爱上了我们这个小家庭
+              </p>
+              <div class="accordion-bottom">
+                <p class="accordion-circle"></p>
+                <strong>张立</strong>
+                <span>禧多多科技</span>
+              </div>
+            </div> -->
+            <el-carousel
+              :interval="3000"
+              type="card"
+              height="3.135rem"
+              autoplay="auto"
+              arrow="never"
+              trigger="hover"
+              @change="time()"
+            >
+              <el-carousel-item>
+                <div class="accordion-item">
+                  <p>
+                    从0到1，NICETEAM给予了我太大的帮助，游戏化管理的模式让我从怀疑到信赖到今天的喜欢，团队业绩越来越好，员工也爱上了我们这个小家庭
+                  </p>
+                  <div class="accordion-bottom">
+                    <p class="accordion-circle"></p>
+                    <strong>张立</strong>
+                    <span>禧多多科技</span>
+                  </div>
+                </div>
+                <!-- <h3 class="medium">{{ item }}</h3> -->
+              </el-carousel-item>
+              <el-carousel-item>
+                <div class="accordion-item">
+                  <p>
+                    从0到1，NICETEAM给予了我太大的帮助，游戏化管理的模式让我从怀疑到信赖到今天的喜欢，团队业绩越来越好，员工也爱上了我们这个小家庭
+                  </p>
+                  <div class="accordion-bottom">
+                    <p class="accordion-circle"></p>
+                    <strong>张立</strong>
+                    <span>禧多多科技</span>
+                  </div>
+                </div>
+                <!-- <h3 class="medium">{{ item }}</h3> -->
+              </el-carousel-item>
+              <el-carousel-item>
+                <div class="accordion-item">
+                  <p>
+                    从0到1，NICETEAM给予了我太大的帮助，游戏化管理的模式让我从怀疑到信赖到今天的喜欢，团队业绩越来越好，员工也爱上了我们这个小家庭
+                  </p>
+                  <div class="accordion-bottom">
+                    <p class="accordion-circle"></p>
+                    <strong>张立</strong>
+                    <span>禧多多科技</span>
+                  </div>
+                </div>
+                <!-- <h3 class="medium">{{ item }}</h3> -->
+              </el-carousel-item>
+            </el-carousel>
+          </div>
+        </div>
+      </div>
+      <!-- 两边盒子
+      <div class="box"></div>
+      <div class="box" style="right:0;"></div> -->
+      <!-- 底部 -->
+      <Footer :footerData="footerData" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Footer from "../components/Footer";
+
+export default {
+  name: "CustomerCase",
+  components: {
+    Footer,
+  },
+  data() {
+    return {
+      footerData: {
+        title: "限时免费！ 立刻定制企业管理解决方案",
+      },
+      auto: true,
+    };
+  },
+  methods: {
+    goAbout() {
+      this.$router.replace("/aboutus");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.customercase {
+  font-size: 0.16rem;
+  .border {
+    width: 88.88%;
+    margin-top: 0.5rem;
+    margin-left: 6%;
+    border: 0.01rem solid #000;
+    position: relative;
+    // 头部
+    .header {
+      width: 100%;
+      height: 3.5rem;
+      color: #fff;
+      background-color: #06f;
+      .headerTop {
+        width: 60%;
+        margin-left: 21%;
+        padding-top: 0.3rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .logo {
+          width: 0.7rem;
+          height: 0.55rem;
+        }
+        .bar {
+          width: 60%;
+          font-weight: bold;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          a {
+            text-decoration: none;
+          }
+          a:hover{
+            background-color: rgb(9, 157, 243);
+            
+          }
+          a:link {
+            color: #fff;
+          }
+          a:visited {
+            color: #fff;
+          }
+        }
+        .mianfei {
+          width: 0.9rem;
+          height: 0.3rem;
+          font-size: 0.14rem;
+          color: #fff;
+          border: 0.01rem solid #fff;
+          border-radius: 0.05rem;
+          background-color: #7fb2ff;
+          margin-right: 0.4rem;
+          outline: none;
+        }
+        .mianfei:active {
+          border: 0.01rem solid #ccc;
+        }
+      }
+      .headerMain {
+        width: 60%;
+        height: 2.7rem;
+        margin-left: 20%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        dl {
+          dt {
+            font-size: 0.24rem;
+            font-weight: bold;
+            text-align: center;
+            margin-bottom: 0.15rem;
+            letter-spacing: 0.02rem;
+          }
+          dd {
+            font-size: 0.16rem;
+            letter-spacing: 0.01rem;
+            text-align: left;
+            margin-bottom: 0.4rem;
+          }
+        }
+        .knowMore {
+          width: 1.5rem;
+          height: 0.4rem;
+          color: #06f;
+          font-size: 0.18rem;
+          letter-spacing: 0.02rem;
+          background-color: #fff;
+          border: 0;
+          border-radius: 0.05rem;
+        }
+      }
+    }
+    // 选择NICETEAM的理由
+    .reason {
+      height: 18.8rem;
+      margin-bottom: 1.2rem;
+      .reasonT {
+        h5 {
+          font-size: 0.3rem;
+          display: block;
+          padding-top: 0.5rem;
+          padding-bottom: 0.15rem;
+        }
+        .question {
+          width: 40%;
+          margin-left: 30%;
+          color: #66667f;
+          margin-bottom: 0.2rem;
+        }
+        .case {
+          width: 80%;
+          margin-left: 10%;
+          height: 3.4rem;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          img {
+            width: 3.5rem;
+            height: 2.3rem;
+            border: 1px solid #ccc;
+          }
+          .told {
+            width: 3rem;
+            height: 2.3rem;
+            p {
+              width: 2rem;
+            }
+            .fs50 {
+              font-size: 0.5rem;
+            }
+            .left {
+              text-align: left;
+            }
+            .right {
+              text-align: right;
+            }
+            .person {
+              display: flex;
+              justify-content: space-between;
+              strong {
+                font-weight: bold;
+                font-size: 0.2rem;
+              }
+            }
+          }
+        }
+      }
+      .reasonM {
+        height: 4.3rem;
+        border: 0.3rem solid #ccc;
+        margin-top: 0.6rem;
+        margin-left: -0.02rem;
+        margin-right: -0.01rem;
+        .content {
+          width: 98%;
+          margin-left: 1%;
+          display: flex;
+          justify-content: space-around;
+          margin-top: 0.6rem;
+          .content-item {
+            width: 2.9rem;
+            height: 3.46rem;
+            background-color: #fff;
+            z-index: 999;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+            dl {
+              width: 1.45rem;
+              dt {
+                font-size: 0.18rem;
+                margin-top: -1.4rem;
+              }
+              dd {
+                text-align: left;
+                margin-top: 0.2rem;
+              }
+            }
+            .circle-item {
+              width: 0.8rem;
+              height: 0.8rem;
+              border: 0.02rem solid #878787;
+              border-radius: 50%;
+              margin-bottom: 1.3rem;
+            }
+          }
+        }
+        .reason-box {
+          width: 94%;
+          margin-left: 1.5%;
+          height: 1.75rem;
+          background-color: #169bd5;
+          position: absolute;
+          top: 15.3rem;
+          left: 0.2rem;
+          .reason-box-item {
+            width: 1.83rem;
+            height: 1.83rem;
+            background-color: #169bd5;
+            transform: rotate(30deg);
+            position: absolute;
+            top: -0.59rem;
+            left: 3.1rem;
+          }
+          .item1 {
+            transform: rotate(-30deg);
+            left: 0.34rem;
+          }
+          .item3 {
+            transform: rotate(-30deg);
+            left: 8.4rem;
+          }
+          .item4 {
+            left: 11.2rem;
+          }
+        }
+      }
+      .reasonB {
+        h5 {
+          font-size: 0.3rem;
+          display: block;
+          letter-spacing: 0.02rem;
+          padding-bottom: 0.05rem;
+          font-weight: bold;
+          margin-top: 0.6rem;
+          margin-bottom: 0.05rem;
+        }
+        .zhuli {
+          margin-bottom: 0.5rem;
+        }
+        .accordion {
+          width: 70%;
+          margin-left: 15%;
+          height: 3.35rem;
+          margin-top: 0.3rem;
+          overflow: hidden;
+          .accordion-item {
+            width: 100%;
+            // margin-left: 15%;
+            height: 100%;
+            background-color: #fff;
+            border: 0.02rem solid #ebebeb;
+            border-radius: 0.3rem;
+            p {
+              margin-top: 0.3rem;
+              width: 66%;
+              margin-left: 17%;
+              text-align: left;
+              font-size: 0.2rem;
+              line-height: 0.42rem;
+            }
+            .accordion-bottom {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              //   margin-top: 0.1rem;
+              .accordion-circle {
+                width: 0.6rem;
+                height: 0.6rem;
+                border: 0.02rem solid #878787;
+                border-radius: 50%;
+                margin-top: 0;
+              }
+              strong {
+                font-weight: bold;
+                font-size: 0.26rem;
+                margin-left: 0.9rem;
+              }
+              span {
+                font-size: 0.2rem;
+                margin-right: 0.9rem;
+              }
+            }
+          }
+        }
+      }
+    }
+    // // 两边盒子
+    // .box {
+    //   width: 1.35rem;
+    //   height: 2.245rem;
+    //   background-color: #f00;
+    //   position: absolute;
+    //   top: 8rem;
+    // }
+  }
+}
+</style>
---
+++ .history/src/views/solutions/Manage_20210108105413.vue
@@ -0,0 +1,434 @@
+<template>
+  <div class="productintroduction">
+    <div class="border">
+      <!-- 头部 -->
+      <div class="header">
+        <div class="headerTop">
+          <p class="bar">
+            <img class="logo" src="../../assets/img/logo.png" alt />
+            <a href="http://localhost:8080/#/">首页</a>
+            <a href="http://localhost:8080/#/productintroduction">产品介绍</a>
+            <a href="http://localhost:8080/#/solutions">解决方案</a>
+            <a href="http://localhost:8080/#/customercase">客户案例</a>
+            <a href="http://localhost:8080/#/aboutus">关于</a>
+          </p>
+          <button class="mianfei">免费试用</button>
+        </div>
+        <div class="headerMain">
+          <div class="theme">
+            <dl>
+              <dt>管理也能玩出味？</dt>
+              <dd>NICETEAM 让管理不再沉闷，让工作玩味不停</dd>
+            </dl>
+            <button class="knowMore" @click="goAbout()">立刻试用</button>
+          </div>
+        </div>
+      </div>
+      <!-- 导航 -->
+      <div class="nav">
+        <div
+          class="nav-item"
+          @click="
+            On1();
+            goProductIntroduction();
+          "
+        >
+          <p class="nav-circle"></p>
+          <span :class="isOn1">全频道获客</span>
+        </div>
+        <div
+          class="nav-item"
+          @click="
+            On2();
+            goTransform();
+          "
+        >
+          <p class="nav-circle"></p>
+          <span :class="isOn2">私域流量转换</span>
+        </div>
+        <div
+          class="nav-item"
+          @click="
+            On3();
+            goManage();
+          "
+        >
+          <p class="nav-circle"></p>
+          <span :class="isOn3">销售客资管理</span>
+        </div>
+      </div>
+      <!-- OneStop -->
+      <div class="oneStop">
+        <div class="top">
+          <h5>为销售赋能，为团队添力</h5>
+          <p class="question">
+            让玩不再是儿戏，让游戏化管理为销售赋能
+          </p>
+          <div class="case">
+            <dl>
+              <dt>
+                <img src="../../assets/img/01.png" alt />
+                <div class="title" style="margin-right: 0.3rem">
+                  <p>SCRM</p>
+                  <p>优化客户管理</p>
+                </div>
+              </dt>
+              <dd>
+                通过销售数据，根据用户行为定义用户画像。利用数据重新定义用户管理，赋能企业销售全新活力。
+              </dd>
+            </dl>
+            <img src="../../assets/img/guanli-1.png" />
+          </div>
+          <div class="f9">
+            <div class="case">
+              <img src="../../assets/img/guanli-2.png" />
+              <dl>
+                <dt>
+                  <img src="../../assets/img/02.png" alt />
+                  <div class="title">
+                    <p>数据分析</p>
+                    <p>让销售实时高效</p>
+                  </div>
+                </dt>
+                <dd>
+                  让数据说话，让销售先行。实时把握用户行为，关注企业转化情况，跟进员工销售行为。让管理有效透明，让销售有迹可循。
+                </dd>
+              </dl>
+            </div>
+          </div>
+          <div class="case">
+            <dl>
+              <dt>
+                <img src="../../assets/img/03.png" alt />
+                <div class="title">
+                  <p>精细化管理</p>
+                  <p>比用户先行一步</p>
+                </div>
+              </dt>
+              <dd>
+                实时记录用户状态，为企业实时推送用户行为记录，为公司业绩添一把力，助力提高员工积极性。
+              </dd>
+            </dl>
+            <img src="../../assets/img/guanli-3.png" />
+          </div>
+          <div class="f9">
+            <div class="case">
+              <img src="../../assets/img/guanli-4.png" />
+              <dl>
+                <dt>
+                  <img src="../../assets/img/04.png" alt />
+                  <div class="title">
+                    <p>数据沉淀</p>
+                    <p>提供持久生命力</p>
+                  </div>
+                </dt>
+                <dd>
+                  赋予每一笔订单全新生命力，让数据成为企业最坚实的基础，用管理给予企业最强盛的生命力。
+                </dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+        <div class="bottom">
+          <div class="comer">
+            <dl>
+              <dt>10000+中小企业的信赖选择</dt>
+              <dd>
+                已覆盖教育、企业服务、互联网、地产家装、招商加盟、医美健康、汽车等行业
+              </dd>
+            </dl>
+            <img src="../../assets/img/comer.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <!-- 底部 -->
+      <Footer :footerData="footerData" />
+    </div>
+  </div>
+</template>
+
+<script>
+// import Header from "../components/Header";
+import Footer from "../../components/Footer";
+
+export default {
+  name: "ProductIntroduction",
+  components: {
+    // Header,
+    Footer,
+  },
+  data() {
+    return {
+      url: require("../../assets/img/fuli.png"),
+      headerData: {
+        free: "免费试用",
+        title: "管理也能玩出味 ?",
+        content: "",
+        detail:
+          "NICETEAM : 让管理不再沉闷，工作玩味不停，全国上千家中小企业的销售福星",
+        immediately: "立即咨询",
+        path: "1",
+      },
+      footerData: {
+        title: "限时免费！ 距离销冠团队只差一步",
+      },
+      isOn1: "",
+      isOn2: "",
+      isOn3: "on",
+    };
+  },
+  methods: {
+    goAbout() {
+      this.$router.replace("/aboutus");
+    },
+    goProductIntroduction() {
+      this.$router.replace("/productintroduction");
+    },
+    goTransform() {
+      this.$router.replace("/transform");
+    },
+    goManage() {
+      this.$router.replace("/manage");
+    },
+    On1() {
+      this.isOn1 = "on";
+      this.isOn2 = "";
+      this.isOn3 = "";
+    },
+    On2() {
+      this.isOn1 = "";
+      this.isOn2 = "on";
+      this.isOn3 = "";
+    },
+    On3() {
+      this.isOn1 = "";
+      this.isOn2 = "";
+      this.isOn3 = "on";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.productintroduction {
+  font-size: 0.16rem;
+  .border {
+    width: 88.88%;
+    margin-top: 0.5rem;
+    margin-left: 6%;
+    border: 0.01rem solid #000;
+    // 头部
+    .header {
+      width: 100%;
+      height: 4.1rem;
+      color: #fff;
+      background-color: #06f;
+      .headerTop {
+        width: 60%;
+        margin-left: 21%;
+        padding-top: 0.3rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .logo {
+          width: 0.7rem;
+          height: 0.55rem;
+        }
+        .bar {
+          width: 60%;
+          font-weight: bold;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          a {
+            text-decoration: none;
+          }
+          a:link {
+            color: #fff;
+          }
+          a:visited {
+            color: #fff;
+          }
+        }
+        .mianfei {
+          width: 1.2rem;
+          height: 0.4rem;
+          font-size: 0.18rem;
+          color: #fff;
+          border: 0.01rem solid #fff;
+          border-radius: 0.05rem;
+          background-color: #06f;
+          margin-right: 0.4rem;
+          outline: none;
+        }
+        .mianfei:active {
+          border: 0.01rem solid #ccc;
+        }
+      }
+      .headerMain {
+        width: 60%;
+        height: 2.7rem;
+        margin-left: 20%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        dl {
+          dt {
+            font-size: 0.24rem;
+            font-weight: bold;
+            text-align: center;
+            margin-bottom: 0.15rem;
+            letter-spacing: 0.02rem;
+          }
+          dd {
+            font-size: 0.16rem;
+            letter-spacing: 0.01rem;
+            text-align: left;
+            margin-bottom: 0.4rem;
+          }
+        }
+        .knowMore {
+          width: 1.5rem;
+          height: 0.4rem;
+          color: #06f;
+          font-size: 0.18rem;
+          letter-spacing: 0.02rem;
+          background-color: #fff;
+          border: 0;
+          border-radius: 0.05rem;
+          outline: none;
+        }
+        .knowMore:active {
+          background-color: #f63;
+          color: #fff;
+        }
+      }
+    }
+    // 导航
+    .nav {
+      width: 60%;
+      margin-left: 20%;
+      margin-top: -0.3rem;
+      height: 0.6rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #fff;
+      border: 0.01rem solid #ccc;
+      border-radius: 0.1rem;
+      .nav-item {
+        width: 20%;
+        margin-left: 7%;
+        margin-right: 7%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .nav-circle {
+          width: 0.3rem;
+          height: 0.3rem;
+          border: 0.02rem solid #ccc;
+          border-radius: 50%;
+        }
+        span {
+          letter-spacing: 0.01rem;
+        }
+      }
+      .on {
+        color: #06f;
+      }
+    }
+    // OneStop
+    .oneStop {
+      .top {
+        h5 {
+          font-size: 0.3rem;
+          display: block;
+          padding-top: 0.5rem;
+          padding-bottom: 0.15rem;
+        }
+        .question {
+          width: 40%;
+          margin-left: 30%;
+          color: #000;
+          font-size: 0.2rem;
+          margin-bottom: 0.6rem;
+          letter-spacing: 0.01rem;
+        }
+        .f9 {
+          width: 100%;
+          height: 3.2rem;
+          // padding-top: 0.2rem;
+          background-color: #f9f9f9;
+        }
+        .case {
+          width: 70%;
+          margin-left: 15%;
+          height: 3.4rem;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          .sort {
+            color: #06f;
+            font-size: 0.4rem;
+            font-weight: bolder;
+          }
+          img {
+            width: 3.5rem;
+            height: 2.3rem;
+            border: 1px solid #ccc;
+          }
+          dl {
+            height: 60%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            dt {
+              // text-align: left;
+              display: flex;
+              justify-content: space-between;
+              img {
+                width: 0.6rem;
+                height: 0.6rem;
+                border: 0;
+              }
+              .title {
+                // margin-right: 0.25rem;
+                p {
+                  text-align: left;
+                  font-size: 0.24rem;
+                  font-weight: bolder;
+                  letter-spacing: 0.01rem;
+                  color: #1b1b1b;
+                }
+              }
+            }
+            dd {
+              width: 2.6rem;
+              text-align: left;
+              line-height: 0.25rem;
+            }
+          }
+        }
+      }
+      .bottom {
+        margin-top: 1.5rem;
+        margin-bottom: 0.5rem;
+        .comer {
+          margin-top: 0.15rem;
+          dl {
+            margin-bottom: 0.2rem;
+            dt {
+              font-size: 0.2rem;
+              font-weight: bold;
+              margin-bottom: 0.1rem;
+            }
+          }
+          img {
+            width: 8.6rem;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
